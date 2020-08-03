fe70ded50c454d3c513d5bdb6075f18a7a8d8d9a
Alena Zachová <azachova@gmail.com>
2020-08-03 11:20:04 +0200

    all

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Alena Zachová");
+
+
+$('a.sipka, .swipe').on('click', function(e) {
+  let cil = $(this).attr('href');
+  let rychlost = 1000;
+  let menu = $('.hore');
+
+  $("html, body").stop().animate(
+      { scrollTop: $(cil).offset().top},
+  rychlost);
+
+  e.preventDefault();
+});
+
+$('a.pre-sipka, .pre-swipe').on('click', function(e) {
+  let cil = $(this).attr('href');
+  let rychlost = 1000;
+  let menu = $('.pre-footer');
+
+  $("html, body").stop().animate(
+      { scrollTop: $(cil).offset().top},
+  rychlost);
+
+  e.preventDefault();
