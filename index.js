6b82070a49f2b7eb1232a5ea010d17ce9b267ccb
alenazachova <ala.zachova@gmail.com>
2025-07-03 00:40:08 +0200

    update: changes in 2025

--- index.js
+++ index.js
@@ -1,64 +1,70 @@
 const reviews = [
   {
     id: 1,
-    name: "Ing. David Šimůnek, PhD.",
-    job: "Head of solutions - Kyndryl",
-    text: "„I had the pleasure of working with Alena on multiple technological implementations. She was a great value and support while developing a solution for our most demanding customer. She was able to cover a broad role from requirements gathering, their translation into the target solution and the actual implementation. Her desire for proficiency and education makes her an asset to a team.”",
+    name: 'Antonín Pleskač',
+    job: 'Frontend & Backend developer - FakturaOnline',
+    text: '„Ala is an energetic and motivated young woman who has recently embarked on her journey into coding and front-end development. Although she’s just beginning, she’s a quick learner, and her passion for the field propels her forward. During our collaboration, she proved to be conscientious and detail-oriented, especially when it came to editing content and texts—tasks that require patience and a keen eye. I believe Ala would thrive in a team that provides the right guidance and camaraderie, allowing her to fully develop her potential and grow further. If you’re looking for a reliable colleague with a strong enthusiasm for new technologies, Ala could be the perfect fit."',
   },
   {
     id: 2,
-    name: "Ing. Lukáš Marek",
-    job: "Majitel firmy - Mpox",
-    text: '„Skvělá a rychlá domluva, web zpracován přesně podle našich představ dle předloženého zadání. Velmi potěšila i blesková odezva při dokončování práce a ladění detailů. Díky!"',
+    name: 'Ing. David Šimůnek, PhD.',
+    job: 'Head of solutions - Kyndryl',
+    text: '„I had the pleasure of working with Alena on multiple technological implementations. She was a great value and support while developing a solution for our most demanding customer. She was able to cover a broad role from requirements gathering, their translation into the target solution and the actual implementation. Her desire for proficiency and education makes her an asset to a team.”',
   },
   {
     id: 3,
-    name: "Michaela Kolková",
-    job: "Majitelka salonu - Beauty Misha",
+    name: 'Ing. Lukáš Marek',
+    job: 'Majitel firmy - Mpox',
+    text: '„Skvělá a rychlá domluva, web zpracován přesně podle našich představ dle předloženého zadání. Velmi potěšila i blesková odezva při dokončování práce a ladění detailů. Díky!"',
+  },
+  {
+    id: 4,
+    name: 'Michaela Kolková',
+    job: 'Majitelka salonu - Beauty Misha',
     text: '„S Alcou jsem byla velice spokojená, perfektní komunikace a skvělá spolupráce. Přesně vystihla mou představou o webových stránkách. Je to profesionál a její práce vypadá úžasně. Mohu jedině doporučit. A budu ráda za další spolupráci."',
   },
 ];
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener('DOMContentLoaded', (event) => {
   //the event occurred
-  console.log("Alena Zachová - CV");
+  console.log('Alena Zachová - CV');
 
   document
-    .querySelector(".intro__mouse-opposite")
-    .addEventListener("click", (e) => {
+    .querySelector('.intro__mouse-opposite')
+    .addEventListener('click', (e) => {
       e.preventDefault();
       const offsetTop = 0;
 
       scroll({
         top: offsetTop,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
 
-  document.querySelector(".intro__mouse").addEventListener("click", (e) => {
+  document.querySelector('.intro__mouse').addEventListener('click', (e) => {
     e.preventDefault();
-    const href = document.querySelector(".intro__mouse").getAttribute("href");
+    const href = document.querySelector('.intro__mouse').getAttribute('href');
     const offsetTop = document.querySelector(href).offsetTop;
 
     scroll({
       top: offsetTop,
-      behavior: "smooth",
+      behavior: 'smooth',
     });
   });
 });
 
-let text = document.querySelector("#text");
-let author = document.querySelector("#name");
-let job = document.querySelector("#job");
+let text = document.querySelector('#text');
+let author = document.querySelector('#name');
+let job = document.querySelector('#job');
 
 let currentItem = 0;
 
-let prevBtn = document.querySelector(".prev-btn");
-let nextBtn = document.querySelector(".next-btn");
+let prevBtn = document.querySelector('.prev-btn');
+let nextBtn = document.querySelector('.next-btn');
 
 function loading() {
   showPerson(currentItem);
 }
-window.addEventListener("DOMContentLoaded", loading);
+window.addEventListener('DOMContentLoaded', loading);
 
 function increase() {
   currentItem++;
@@ -67,8 +73,8 @@ function increase() {
   }
   showPerson(currentItem);
 }
-nextBtn.addEventListener("click", increase);
-prevBtn.addEventListener("click", down);
+nextBtn.addEventListener('click', increase);
+prevBtn.addEventListener('click', down);
 
 function down() {
   currentItem--;
